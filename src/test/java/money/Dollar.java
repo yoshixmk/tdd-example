604d83679d25b798f02a9b809f6dcd53750cf75b
YoshihiroUeki <pirori67@gmail.com>
2019-03-31 13:45:30 +0900

    eq test pass

--- src/test/java/money/Dollar.java
+++ src/test/java/money/Dollar.java
@@ -2,14 +2,19 @@ package money;
 
 public class Dollar {
 
-	public int mount;
+	public int amount;
 	
 	public Dollar(int mount) {
-		this.mount = mount;
+		this.amount = mount;
 	}
 
 	public Dollar times(int multiplier) {
-		return new Dollar(this.mount * multiplier);
+		return new Dollar(this.amount * multiplier);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return this.amount == ((Dollar)obj).amount;
 	}
 
 }
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -11,9 +11,9 @@ class MoneyTest {
 	void testMultiplication() {
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
-		assertEquals(10, product.mount);
+		assertEquals(10, product.amount);
 		product = five.times(3);
-		assertEquals(15, product.mount);
+		assertEquals(15, product.amount);
 	}
 	
 	@Test
