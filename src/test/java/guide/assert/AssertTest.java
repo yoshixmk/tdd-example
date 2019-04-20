4f883c343b7e1eab7d9c2483f0429fea0bf530a5
Yoshihiro Ueki <pirori67@gmail.com>
2019-04-21 08:51:23 +0900

    other assertion

---
+++ src/test/java/guide/assert/AssertTest.java
@@ -0,0 +1,42 @@
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofMinutes;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+
+class AssertionsDemo {
+
+    @Test
+    @DisplayName("1分未満で終わるタスクであること")
+    void timeoutNotExceeded() {
+        String actualResult = assertTimeout(ofMinutes(1), () -> {
+            return "result";
+        });
+        assertEquals("result", actualResult);
+    }
+
+    @Test
+    @Disabled("assertTimeoutの落ちるテスト")
+    void timeoutExceeded() {
+        assertTimeout(ofMillis(10), () -> {
+            Thread.sleep(100);
+        });
+    }
+
+    @Test
+    @Disabled("assertTimeoutの落ちるテスト2")
+    void timeoutExceededWithPreemptiveTermination() {
+        assertTimeoutPreemptively(ofMillis(10), () -> {
+            Thread.sleep(100);
+        });
+    }
+}
