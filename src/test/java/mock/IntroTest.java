686237da303b924862f1f43bc5a21c603dabb322
YoshihiroUeki <pirori67@gmail.com>
2019-05-04 14:01:49 +0900

    refactor

--- src/test/java/mock/IntroTest.java
+++ src/test/java/mock/IntroTest.java
@@ -3,11 +3,19 @@ package mock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.LinkedList;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
@@ -16,11 +24,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class IntroTest {
-	
-	@Mock private LinkedList<String> mockedList;
-	@Mock private LinkedList<String> mockedList2;
-	@Mock private LinkedList<String> mockedList3;
-	
+
+	@Mock
+	private LinkedList<String> mockedList;
+	@Mock
+	private LinkedList<String> mockedList2;
+	@Mock
+	private LinkedList<String> mockedList3;
+
 	@Test
 	void 初めのテスト() {
 		mockedList.add("one");
@@ -80,7 +91,7 @@ class IntroTest {
 		doThrow(new RuntimeException()).when(mockedList).clear();
 		assertThrows(RuntimeException.class, () -> mockedList.clear());
 	}
-	
+
 	@Test
 	void 影響を与えないテスト() {
 		mockedList.add("one");
